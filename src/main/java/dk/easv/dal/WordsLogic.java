ac56922c7a056692ab0d0b5c8faa476a3e5d56c9
RasWChr <r.west.chr@gmail.com>
2025-10-28 11:21:42 +0100

    Words logic

--- src/main/java/dk/easv/dal/WordsLogic.java
+++ src/main/java/dk/easv/dal/WordsLogic.java
@@ -1,5 +1,11 @@
 package dk.easv.dal;
 
+import java.util.List;
+
 public class WordsLogic {
-    
-}
+    private WordsData data = new WordsData();
+    public List<String> getWords(){
+        return data.getWords();
+    }
+
+    }
--- src/main/java/dk/easv/gui/HelloController.java
+++ src/main/java/dk/easv/gui/HelloController.java
@@ -1,14 +1,12 @@
 package dk.easv.gui;
 
+import dk.easv.dal.WordsLogic;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+        //FXML referencer
+        private WordsLogic wordsLogic = new WordsLogic();
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+
 }
